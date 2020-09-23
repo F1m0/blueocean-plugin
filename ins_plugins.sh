a755ba3f9ad752ae91da86efdc266cd1f754b679
S J <nictsj@gmail.com>
2020-09-23 14:19:42 +0200

    update JF

--- ins_plugins.sh
+++ ins_plugins.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-cp -r blueocean/target/plugins /usr/share/jenkins/ref/plugins/   ;
+cp  blueocean/target/plugins/* /usr/share/jenkins/ref/plugins/   ;
 for f in /usr/share/jenkins/ref/plugins/*.hpi; do mv "$f" "${f%%hpi}jpi"; done;
 install-plugins.sh antisamy-markup-formatter matrix-auth;
 for f in /usr/share/jenkins/ref/plugins/blueocean-*.jpi; do mv "$f" "$f.override"; done;
