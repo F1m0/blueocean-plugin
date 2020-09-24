fcdabdfea58b7e6c282d01473fb5437c273f5345
S J <nictsj@gmail.com>
2020-09-24 11:14:40 +0200

    script upd

--- ins_plugins.sh
+++ ins_plugins.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
+echo "copy plugins"
 cp  blueocean/target/plugins/* /usr/share/jenkins/ref/plugins/   ;
+echo "rename to jpi"
 for f in /usr/share/jenkins/ref/plugins/*.hpi; do mv "$f" "${f%%hpi}jpi"; done;
+echo "install plugins"
 install-plugins.sh antisamy-markup-formatter matrix-auth;
+echo "override"
 for f in /usr/share/jenkins/ref/plugins/blueocean-*.jpi; do mv "$f" "$f.override"; done;
